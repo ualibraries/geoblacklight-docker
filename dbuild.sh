db54e2c8f584a8ed7e845baa27a49c0530a1c323
kev1njodea <kevinjodea@arizona.edu>
2024-01-16 08:49:13 -0700

    Adjusted config to run on a VM with reverse proxy

--- dbuild.sh
+++ dbuild.sh
@@ -5,7 +5,7 @@ source .env
 
 cd docker
 
-docker build --rm=true -t ual-goblight:latest \
+docker build --no-cache -t ual-goblight:latest \
     --build-arg RUBY_VERSION=${RUBY_VERSION} \
     --build-arg RAILS_VERSION=${RAILS_VERSION} \
     --build-arg VIEW_COMPONENT_VERSION=${VIEW_COMPONENT_VERSION} \
@@ -15,7 +15,7 @@ docker build --rm=true -t ual-goblight:latest \
     --build-arg USER_UID=${DUID} \
     -f app/Dockerfile .
 
-docker build --rm=true -t ual-solr:latest \
+docker build --no-cache -t ual-solr:latest \
     --build-arg SOLR_VERSION=${SOLR_VERSION} \
     --build-arg SOLR_USER=${SOLR_USER} \
     --build-arg SOLR_PASS=${SOLR_PASS} \
